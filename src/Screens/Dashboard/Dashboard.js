79a0f64b9819d023667ad16d22e4100bb403b108
Connor <kerrigan.connor@googlemail.com>
2021-03-02 17:29:55 +0000

    Monke

--- src/Screens/Dashboard/Dashboard.js
+++ src/Screens/Dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import logo from "../../logo.svg";
 import monkey from "../../monkey.svg";
 import styles from "./DashboardStyles";
 
--- src/Screens/Dashboard/DashboardStyles.js
+++ src/Screens/Dashboard/DashboardStyles.js
@@ -1,10 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { keyframes } from "styled-components";
 
-const spin = keyframes`
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-`;
 const styles = makeStyles({
   Dashboard: { display: "flex", alignContent: "center", justifyContent: "center", height: '100vh' },
   Applogo: {
