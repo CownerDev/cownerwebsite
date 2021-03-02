6e785f5ff07af0b7b8a33793db8370b9b985e755
Connor <kerrigan.connor@googlemail.com>
2021-03-02 22:54:46 +0000

    Removed div

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,11 +2,7 @@ import React from "react";
 import Logo from "../Logo/Logo";
 
 const Header = () => {
-  return (
-    <div classNam="Appheader">
-      <Logo />
-    </div>
-  );
+  return <Logo />;
 };
 
 export default Header;
